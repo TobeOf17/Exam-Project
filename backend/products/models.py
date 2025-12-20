41765493db4a569c50036e3c7ebb8251beb55ab1
Nathan <nathanakpe3@gmail.com>
2025-12-20 09:00:31 +0100

    Add: product,SKU

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class SKU(models.Model):
+    
+    product = models.ForeignKey(Product, related_name='skus', on_delete=models.CASCADE) [cite: 6]
+    sku_code = models.CharField(max_length=100, unique=True)
+    barcode = models.CharField(max_length=100, unique=True) 
+    base_price = models.DecimalField(max_digits=10, decimal_places=2) 
+
+    def __str__(self):
+        return f"{self.product.name} - {self.sku_code}"
